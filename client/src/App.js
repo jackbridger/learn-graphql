7eddb4db8f5539b801ab506cb0ddd2566db3f650
Jack Bridger <jacksbridger@gmail.com>
2019-12-05 20:05:09 +0000

    setup the client

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css';
+import './App.css';
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+function App() {
+  return (
+    <Router>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Register() {
+    return (
+        <div>
+            <h1>Register page</h1>
+        </div>
+    )
+}
